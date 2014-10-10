4642dd91ed8a4ea24b032f78cb26ba1857b10a57
Dennis Qian <dennis@airseed.com>
2014-10-10 12:12:29 -0700

    adding configurable base urls

--- lib/airseed/user_api.rb
+++ lib/airseed/user_api.rb
@@ -9,13 +9,15 @@ module Airseed
     end
 
     def users_info(id = 'me')
-      request(:get, request_url("v1/users/#{id}"), { bearer_token: @access_token })
+      url = request_url("v1/users/#{id}")
+      request(:get, request_url(url), { bearer_token: @access_token })
     end
 
     def users_data(endpoint, params = {})
       params.merge!(bearer_token: @access_token)
       id = params.delete(:id) || 'me'
-      request(:get, request_url("v1/users/#{id}/#{endpoint}"), params)
+      url = request_url("v1/users/#{id}/#{endpoint}")
+      request(:get, url, params)
     end
 
     def endpoint(path, params = {})
@@ -25,7 +27,7 @@ module Airseed
 
     private
     def request_url(path)
-      URI.join(API_BASE_URL, path).to_s
+      URI.join(Airseed.api_base_url || API_BASE_URL, path).to_s
     end
   end
 end
